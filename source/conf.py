e4e2c4d924d7fe1b1ae0fa802930a71357c2fb30
Eugen Ciur <eugen@papermerge.com>
2021-08-07 08:30:13 +0200

    moving towards sphinx_rtd_theme

--- source/conf.py
+++ source/conf.py
@@ -51,15 +51,3 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-#html_theme_options = {
-#    "nav_include_hidden": True,
-#    "show_nav": True,
-#    "show_breadcrumbs": True,
-#    "breadcrumbs_separator": "/",
-#    "show_prev_next": False
-#}
-#html_context = {
-#    'css_files': ['_static/custom.css'],
-#}
-
