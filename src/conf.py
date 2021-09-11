3584243a52616a3169aad9b7cb35a90a24fcbdfa
Eugen Ciur <eugen@papermerge.com>
2021-09-11 13:45:02 +0200

    show multiple documentation versions with sphinx-multiversion

--- src/conf.py
+++ src/conf.py
@@ -28,6 +28,7 @@ author = 'Eugen Ciur'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_multiversion",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
