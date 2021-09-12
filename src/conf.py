d3dbd9736510752ae0b03fbce0398f8dff8604a6
Eugen Ciur <eugen@papermerge.com>
2021-09-12 08:49:30 +0200

    correctly match branches (2.0.x, 2.1.x, 2.2.x, ...,  master)

--- src/conf.py
+++ src/conf.py
@@ -36,7 +36,8 @@ extensions = [
 #  origin/2.0.x   # 2.0.x branch
 #  origin/2.1.x   # 2.1.x branch
 #  origin/master  # master branch
-smv_remote_whitelist = r'^(origin/\d+\.\d+\.[\d+x]|origin/master)$'
+smv_remote_whitelist = r'^(origin)$'  # remote ref format
+smv_branch_whitelist = r'^(\d+\.\d+\.[\d+x]|master)$'  # branch format
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
