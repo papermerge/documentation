7ba4a47367177ee30963f4e644fcc960c478951f
Eugen Ciur <eugen@papermerge.com>
2021-09-12 09:37:04 +0200

    minor adjustments to run.dev.sh scripts

--- run.dev.sh
+++ run.dev.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-IMAGE=${1:-papermerge/documentation-dev}
-PORT=${2:-9012}
+
+# Starts a docker container of given documentation image.
+#
+# Usage:
+#
+# ./run.dev.sh <HOST-PORT> <DOCKER-IMAGE>
+#
+# Example:
+#   # run papermerge/documentation:dev image on port 8888
+#   $ ./run.dev.sh 8888
+#
+#   # run docs:dev image on port 9988
+#   $ ./run.dev.sh 9988 docs:dev
+#
+#   # run papermerge/documentation:dev image on port 9012
+#   # ./run.dev.sh
+
+IMAGE=${2:-papermerge/documentation:dev}
+PORT=${1:-9012}
 
 docker run --rm \
     -p ${PORT}:80 \
